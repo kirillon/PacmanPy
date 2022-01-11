fe506dd276ce914f3fd5af95996d59b2ac1d45a0
kirillon <kirilminecraft@gmail.com>
2022-01-11 19:22:30 +0300

    1

---
+++ settings.py
@@ -0,0 +1,29 @@
+# game settings
+WIDTH = 1200
+HEIGHT = 800
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+DOUBLE_WIDTH = 2 * WIDTH
+DOUBLE_HEIGHT = 2 * HEIGHT
+PENTA_HEIGHT = 5 * HEIGHT
+FPS = 60
+TILE = 100
+
+# player
+player_pos = None
+player_angle = None
+player_speed = None
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 0, 0)
+GREEN = (0, 80, 0)
+BLUE = (0, 0, 255)
+DARKGRAY = (40, 40, 40)
+PURPLE = (120, 0, 120)
+SKYBLUE = (0, 186, 255)
+YELLOW = (220, 220, 0)
+SANDY = (244, 164, 96)
+DARKBROWN = (97, 61, 25)
+DARKORANGE = (255, 140, 0)
